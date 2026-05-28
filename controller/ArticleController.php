f56ec584c8528d228ed1341f7bc75e78e372f66a
kevin-guerrero <kevin.guerrero.7e6@itb.cat>
2026-05-28 08:34:55 +0200

    feat: afegir controller per a articles

---
+++ controller/ArticleController.php
@@ -0,0 +1,38 @@
+<?php 
+include_once  __DIR__ .'dao/articleDao.php';
+
+class ArticleController {
+    private $articleDao;
+
+    public function __construct($connection) {
+        $this->articleDao = new ArticleDao($connection);
+    }
+
+    public function obtenirArticles() {
+        $llistaArticle = $this->articleDao->obtenirArticles();
+        return $llistaArticle;
+    }
+
+    public function obtenirArticlePerId($id) {
+        $article = $this->articleDao->obtenirArticlePerId($id);
+        return $article;
+    }
+
+    public function crearArticle($article) {
+        $this->articleDao->crearArticle($article);
+    }
+
+    public function actualitzarArticle($article) {
+        $this->articleDao->actualitzarArticle($article);
+    }
+
+    public function actualitzarVistaArticle($article) {
+        $this->articleDao->actualitzarVistaArticle($article);
+    }
+
+    public function eliminarArticle($id) {
+        $this->articleDao->deleteArticle($id);
+    }
+}
+
+?>
